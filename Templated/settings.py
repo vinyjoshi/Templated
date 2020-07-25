c5f98ebe6562c5ef02dcf9e09844b4ec73c760b8
Vinay Joshi <viyj2000@gmail.com>
2020-07-25 23:12:13 +0530

    images load

--- Templated/settings.py
+++ Templated/settings.py
@@ -111,7 +111,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'templated/static')
+    os.path.join(BASE_DIR, 'Templated/static')
 ]
 
 # Media Files:
