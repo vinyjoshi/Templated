ef8c6e5a0cfbabef9410febcf895b8b8394b127f
Vinay Joshi <viyj2000@gmail.com>
2020-07-27 15:40:39 +0530

    Final Commit

--- Home/views.py
+++ Home/views.py
@@ -1,27 +1,29 @@
 from django.shortcuts import render
 from django.views import View
 from .forms import ContactForm
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 class Home_view(View):
+    form_class = ContactForm
     template_name = 'Home/Index.html'
 
     def get(self, request):
-        return render(request, self.template_name, {})
+        form = self.form_class()
+        return render(request, self.template_name, {'form':form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+        
 
 class Generic(View):
     template_name = 'Home/generic.html'
 
     def get(self, request):
         return render(request, self.template_name, {})
+  
+    
 
-def Contact(request):
-    form = ContactForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-    
-    context = {
-        'form' : form,
-    }
-    return render(request, 'Home/Index.html', context)    
-    
